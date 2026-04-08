e30e23dac958da0c0f1932b889b03f594f1ca074
Telezi Mciteka <ST10481016@rcconnect.edu.za>
2026-04-08 12:54:58 +0200

    Add files via upload

---
+++ UserProfile.cs
@@ -0,0 +1,21 @@
+﻿public class UserProfile
+{
+
+    public string GetArt()
+    {
+        string art = @"  ______   ______  _____ ____  ____   ___ _____ 
+ / ___\ \ / / __ )| ____|  _ \| __ ) / _ \_   _|
+| |    \ V /|  _ \|  _| | |_) |  _ \| | | || |  
+| |___  | | | |_) | |___|  _ <| |_) | |_| || |  
+ \____| |_| |____/|_____|_| \_\____/ \___/ |_|  ";
+
+        return art;
+    }
+
+
+    public void PrintArt()
+    {
+        Console.WriteLine(GetArt());
+    }
+}
+//Class UserProfile
