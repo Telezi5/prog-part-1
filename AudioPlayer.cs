9788490759c2989c4e356e31f0e1b4885fff6a36
Telezi Mciteka <ST10481016@rcconnect.edu.za>
2026-04-08 12:54:32 +0200

    Add files via upload

---
+++ AudioPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Media;
+
+namespace Prog_part_1.Services
+{
+    public class AudioPlayer
+    {
+        public void Greetings()
+        {
+            try
+            {
+                string path = @"Assets\welcome.wav"; // correct relative path
+                SoundPlayer player = new SoundPlayer(path);
+
+                player.Load();   // ensure the file is loaded before playing
+                player.Play();   // or use PlaySync() if you want it to wait
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error playing sound: {e.Message}");
+            }
+        }
+    }
+}
---
+++ ConsoleUI.cs
@@ -0,0 +1,33 @@
+﻿using Prog_part_1.Services;
+
+public class ConsoleUI
+{
+    private AudioPlayer _audioPlayer;
+    private ResponseServices _responseService;
+    private UserProfile _userProfile;
+    public ConsoleUI()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        _userProfile = new UserProfile();
+        _audioPlayer = new AudioPlayer();
+        _responseService = new ResponseServices();
+    }
+
+    public void Start()
+    {
+        _userProfile.PrintArt();
+        _audioPlayer.Greetings();
+
+        Console.WriteLine("Welcome to CyberBot!");
+
+        while (true)
+        {
+            Console.Write("You:  ");
+            string userInput = Console.ReadLine();
+
+
+            string response = _responseService.GetResponse(userInput);
+            Console.WriteLine("Bot: " + response);
+        }
+    }
+}
